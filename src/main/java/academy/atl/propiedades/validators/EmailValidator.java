1a3b7f190936f1196dda66ff197675bd4941a283
diana <diana.carhuamanta@deister.es>
2023-08-07 00:40:08 -0500

    modify

--- src/main/java/academy/atl/propiedades/validators/EmailValidator.java
+++ src/main/java/academy/atl/propiedades/validators/EmailValidator.java
@@ -1,2 +1,23 @@
-package academy.atl.propiedades.validators;public class EmailValidator {
+package academy.atl.propiedades.validators;
+
+public class EmailValidator {
+    public boolean esValido(String email) {
+        // Chequear que el email tenga un arroba
+        if (!email.contains("@")) {
+            return false;
+        }
+        if (!email.contains(".")) {
+            return false;
+        }
+        if (email.endsWith("@")) {
+            return false;
+        }
+        if (email.contains(" ")) {
+            return false;
+        }
+        if (email.length() > 255) {
+            return false;
+        }
+        return true;
+    }
 }
