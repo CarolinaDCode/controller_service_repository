09b2ade5c454abcdd55bafb371523a0201866c73
diana <diana.carhuamanta@deister.es>
2023-08-07 00:38:30 -0500

    modify

--- src/main/java/academy/atl/propiedades/services/PropiedadService.java
+++ src/main/java/academy/atl/propiedades/services/PropiedadService.java
@@ -1,6 +1,8 @@
 package academy.atl.propiedades.services;
 
 import academy.atl.propiedades.models.Propiedad;
+import academy.atl.propiedades.repository.IPropiedadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,27 +11,32 @@ import java.util.List;
 @Service
 public class PropiedadService {
 
+    @Autowired
+    private IPropiedadRepository repository;
+
     public void agregar(@RequestBody Propiedad propiedad){
         if (propiedad.getTitulo().equals("")){
             return;
         }
-        // Llamar a la base de datos.
+        repository.save(propiedad);
     }
 
     public void modificar(@RequestBody Propiedad propiedad, @PathVariable("id") Long id){
         // lógica para modificar un propiedad que se encuentra en la base de datos
+        repository.save(propiedad);
     }
 
     public void eliminar(@PathVariable("id") Long id){
         // lógica para eliminar un propiedad que se encuentra en la base de datos
+        repository.deleteById(id);
     }
 
 
     public Propiedad get(@PathVariable("id") Long id){
-        return null;
+        return repository.findById(id).get();
     }
 
     public List<Propiedad> getAll(){
-        return null;
+        return (List) repository.findAll();
     }
 }
